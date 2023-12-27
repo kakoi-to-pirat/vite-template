ad98507fb39ceb8d87fec21e8d236a00af47bd94
Пидай Максим Анатольевич <kakoi-to.pirat@yandex.ru>
2023-12-27 22:28:34 +0300

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/vite-template/',
   resolve: {
     alias: {
       '@': '/src',
